09c3fc5ce80316108c6b1f856835ae38e259f170
SergeyBurlaka <anatoliy8827@gmail.com>
2016-10-24 16:47:24 +0300

    Update WineInStock.java

--- app/src/main/java/com/woxapp/go/test/burlaka/winevaultapp/data/model/WineInStock.java
+++ app/src/main/java/com/woxapp/go/test/burlaka/winevaultapp/data/model/WineInStock.java
@@ -3,6 +3,7 @@ package com.woxapp.go.test.burlaka.winevaultapp.data.model;
 /**
  * Created by Operator on 21.10.2016.
  */
+
 public class WineInStock implements Model {
 
     public static final String TABLE = "WineInStockTable";
@@ -16,39 +17,42 @@ public class WineInStock implements Model {
 
     public static final String BOTTLE = "bottle";
 
-
     int total ;
     int inbox;
     int bottle;
 
+    
     public WineInStock(){
-
     }
 
+    
     public WineInStock(
             int total,
             int inbox,
             int bottle
     ) {
-
         this.total = total;
         this.inbox = inbox;
         this.bottle = bottle;
     }
+    
 
     public int getBottle() {
         return bottle;
     }
 
+    
     public void setBottle(String bottle) {
         if(bottle.matches("")) {this.bottle = 0; return;}
         this.bottle = Integer.parseInt(bottle);
     }
 
+    
     public int getTotal() {
         return total;
     }
 
+    
     public void setTotal(String total) {
         if(total.matches("")) {this.total = 0; return;}
         this.total = Integer.parseInt(total);
