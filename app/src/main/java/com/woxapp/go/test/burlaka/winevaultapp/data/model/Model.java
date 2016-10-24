96228e62a4f0c871e02c92a55467d10c32d6aa91
SergeyBurlaka <anatoliy8827@gmail.com>
2016-10-24 16:44:54 +0300

    Update Model.java

--- app/src/main/java/com/woxapp/go/test/burlaka/winevaultapp/data/model/Model.java
+++ app/src/main/java/com/woxapp/go/test/burlaka/winevaultapp/data/model/Model.java
@@ -4,12 +4,12 @@ package com.woxapp.go.test.burlaka.winevaultapp.data.model;
  * Created by Operator on 22.10.2016.
  */
 public interface Model {
+    
     public String getDate();
     public String getWineName();
     public String getText();
     public int getBottleCount();
     public int getBoxCount();
-
     public int getStatus_id();
-
+    
 }
